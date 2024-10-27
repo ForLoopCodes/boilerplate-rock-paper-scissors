2fe6b3e12a1857ced82dd2df78b3d98f63437b2c
Meet Patel <meetnp1706@gmail.com>
2024-10-27 17:37:50 +0000

    first

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-# This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
+from collections import Counter
+import random
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+def ai_player(prev_opponent_play,
+              opponent_history=[],
+              play_order=[{
+                  ''.join([a, b, c, d]): 0 
+                  for a in 'RPS' for b in 'RPS' for c in 'RPS' for d in 'RPS'
+              }]):
+    if not prev_opponent_play:
+        prev_opponent_play = 'P'
+    opponent_history.append(prev_opponent_play)
+    last_four = "".join(opponent_history[-4:])
+    if len(last_four) == 4:
+        play_order[0][last_four] += 1
+    potential_plays = [
+        last_four[1:] + "R",
+        last_four[1:] + "P",
+        last_four[1:] + "S",
+    ]
+    sub_order = {k: play_order[0][k] for k in potential_plays if k in play_order[0]}
+    if sub_order:
+        prediction = max(sub_order, key=sub_order.get)[-1:]
+    else:
+        prediction = random.choice(['R', 'P', 'S'])
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
 
-# Uncomment line below to play interactively against a bot:
-# play(human, abbey, 20, verbose=True)
-
-# Uncomment line below to play against a bot that plays randomly:
-# play(human, random_player, 1000)
-
-
-
-# Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+x = play(ai_player, quincy, 1000)
+a = play(ai_player, abbey, 1000)
+b = play(ai_player, kris, 1000)
+c = play(ai_player, mrugesh, 1000)
